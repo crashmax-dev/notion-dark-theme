771df877a75e0f3ae6b964c583b421cdd8e42f3e
crashmax-dev <crashmax.dev@gmail.com>
2022-11-20 15:23:08 +0000

    Deploying to gh-pages from @ crashmax-dev/notion-dark-theme@0701e9193233ab6bc29338b5909d73c64eef5ea6 🚀

---
+++ notion-dark-theme.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        notion-dark-theme
+// @version     0.0.1
+// @license     MIT
+// @homepage    https://crashmax-dev.github.io/notion-dark-theme/
+// @match       https://*.notion.site/*
+// @updateURL   https://crashmax-dev.github.io/notion-dark-theme/notion-dark-theme.meta.js
+// @downloadURL https://crashmax-dev.github.io/notion-dark-theme/notion-dark-theme.user.js
+// ==/UserScript==
+
+(function(){"use strict"})();
